6c3bb667c18e93ac99f5bb6b79354be266a01d0f
Jason <df41022@gmail.com>
2023-05-20 04:25:47 +0800

    modify homepage

--- homepage/src/app/App.js
+++ homepage/src/app/App.js
@@ -1,22 +1,42 @@
-import React, { Component, Suspense, lazy } from 'react';
+import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import './App.scss';
 
-import Spinner from '../app/Spinner';
-
-const Alarm = lazy(() => import('./alarm'));
 
 class App extends Component {
   componentDidMount() {
   }
   render() {
     return (
-      <div className="main-panel">
-        <div className="content-wrapper">
-          <Suspense fallback={<Spinner />}>
-            <Alarm />
-          </Suspense>
-        </div>
+      <div className="content">
+        <table className="table">
+          <thead>
+            <tr>
+              <th> Reservoir </th>
+              <th> </th>
+              <th> Earthquake </th>
+              <th> </th>
+              <th> Electricity</th>
+              <th> </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <div className="badge badge-outline-low">Low Risk</div>
+              </td>
+              <td> some text some text some text some text some text... </td>
+              <td>
+                <div className="badge badge-outline-medium">Medium Risk</div>
+              </td>
+              <td> some text some text some text some text some text... </td>
+              <td>
+                <div className="badge badge-outline-high">High Risk</div>
+              </td>
+              <td> some text some text some text some text some text... </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
