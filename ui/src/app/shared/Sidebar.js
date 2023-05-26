0c98b45fdfbb87ab60422445dd51ff482d15da95
QL1115 <jane19981115@gmail.com>
2023-05-26 18:22:36 +0800

    change logo and title to tsmc

--- ui/src/app/shared/Sidebar.js
+++ ui/src/app/shared/Sidebar.js
@@ -60,8 +60,8 @@ class Sidebar extends Component {
       <nav className="sidebar sidebar-offcanvas" id="sidebar">
         <div className="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top">
           {/* TODO */}
-          <a className="sidebar-brand brand-logo" href="index.html"><img src={require('../../assets/images/logo.svg')} alt="logo" /></a>
-          <a className="sidebar-brand brand-logo-mini" href="index.html"><img src={require('../../assets/images/logo-mini.svg')} alt="logo" /></a>
+          <a className="sidebar-brand brand-logo" href="index.html"><img src={require('../../assets/images/tsmc_logo_large.svg')} alt="logo" /></a>
+          <a className="sidebar-brand brand-logo-mini" href="index.html"><img src={require('../../assets/images/tsmc_logo.svg')} alt="logo" /></a>
         </div>
         <ul className="nav">
           <li className="nav-item profile">
